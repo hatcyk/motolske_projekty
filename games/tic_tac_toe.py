4218c13b3afe0e616b9dd08bc73442abf370cba2
hatcyk <133507370+hatcyk@users.noreply.github.com>
2026-01-07 10:13:47 +0100

    refactor: extract Tic-Tac-Toe shared logic

--- games/tic_tac_toe.py
+++ games/tic_tac_toe.py
@@ -1,11 +1,17 @@
 """
-tic_tac_toe.py: Implementace hry Tic-tac-toe
+tic_tac_toe.py: CLI implementace hry Tic-tac-toe
 
 author: Štefan Barát
 email: barat70671@mot.sps-dopravni.cz
 discord: hatsukooo
 """
 
+from games.tic_tac_toe_logic import (
+    initialize_board,
+    validate_move,
+    check_winner,
+    check_draw
+)
 from utils.timer import start_timer, stop_timer, format_time
 from utils.statistics import add_tic_tac_toe_result
 
@@ -25,16 +31,6 @@ def display_welcome_and_rules():
     print("Začínáme hru!")
 
 
-def initialize_board():
-    """
-    Inicializuje prázdné hrací pole.
-
-    Returns:
-        list: List 9 mezer reprezentující hrací pole (indexy 0-8)
-    """
-    return [' '] * 9
-
-
 def display_board(board):
     """
     Zobrazí hrací pole v pěkném formátu.
@@ -50,132 +46,6 @@ def display_board(board):
     print("+---+---+---+")
     print(f"| {board[6]} | {board[7]} | {board[8]} |")
     print("+---+---+---+")
-
-
-def validate_move(move_input, board):
-    """
-    Validuje tah hráče.
-
-    Args:
-        move_input (str): Vstup od hráče
-        board (list): Aktuální stav hrací plochy
-
-    Returns:
-        tuple: (is_valid, position, error_message)
-               position je index 0-8 pokud je tah platný
-    """
-    # Kontrola že je vstup číselný
-    if not move_input.isdigit():
-        return False, None, "Vstup musí být číslo!"
-
-    position = int(move_input)
-
-    # Kontrola rozsahu 1-9
-    if position < 1 or position > 9:
-        return False, None, "Číslo musí být mezi 1 a 9!"
-
-    # Konverze na index 0-8
-    index = position - 1
-
-    # Kontrola že pole je volné
-    if board[index] != ' ':
-        return False, None, "Toto pole je již obsazené!"
-
-    return True, index, None
-
-
-def check_horizontal_win(board):
-    """
-    Kontroluje horizontální výhru.
-
-    Args:
-        board (list): Hrací pole
-
-    Returns:
-        str or None: 'X', 'O', nebo None
-    """
-    # Kontrola řádků
-    for i in range(0, 9, 3):
-        if board[i] == board[i+1] == board[i+2] and board[i] != ' ':
-            return board[i]
-    return None
-
-
-def check_vertical_win(board):
-    """
-    Kontroluje vertikální výhru.
-
-    Args:
-        board (list): Hrací pole
-
-    Returns:
-        str or None: 'X', 'O', nebo None
-    """
-    # Kontrola sloupců
-    for i in range(3):
-        if board[i] == board[i+3] == board[i+6] and board[i] != ' ':
-            return board[i]
-    return None
-
-
-def check_diagonal_win(board):
-    """
-    Kontroluje diagonální výhru.
-
-    Args:
-        board (list): Hrací pole
-
-    Returns:
-        str or None: 'X', 'O', nebo None
-    """
-    # Hlavní diagonála (0, 4, 8)
-    if board[0] == board[4] == board[8] and board[0] != ' ':
-        return board[0]
-
-    # Vedlejší diagonála (2, 4, 6)
-    if board[2] == board[4] == board[6] and board[2] != ' ':
-        return board[2]
-
-    return None
-
-
-def check_winner(board):
-    """
-    Kontroluje jestli někdo vyhrál.
-
-    Args:
-        board (list): Hrací pole
-
-    Returns:
-        str or None: 'X', 'O', nebo None
-    """
-    # Zkontrolovat všechny možné způsoby výhry
-    winner = check_horizontal_win(board)
-    if winner:
-        return winner
-
-    winner = check_vertical_win(board)
-    if winner:
-        return winner
-
-    winner = check_diagonal_win(board)
-    if winner:
-        return winner
-
-    return None
-
-
-def check_draw(board):
-    """
-    Kontroluje jestli je remíza.
-
-    Args:
-        board (list): Hrací pole
-
-    Returns:
-        bool: True pokud je remíza, False jinak
-    """
-    return ' ' not in board
 
 
 def play_tic_tac_toe():
